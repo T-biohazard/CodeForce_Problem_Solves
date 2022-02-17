2889c85de234cf773e014d14c9e08d0fe9c65cce
Tahsin <81536995+T-biohazard@users.noreply.github.com>
2022-02-17 19:23:22 +0600

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -3,3 +3,20 @@
 //19 , sizeof()-byte ruturn kore
 
 //do a Marksheet code....
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+using namespace std;
+int main(){
+int a,b;
+float c,d;
+
+cin>>a>>b;
+cin>>c>>d;
+
+cout<<a+b<<" "<<a-b<<endl;
+
+cout<<showpoint;
+cout<<setprecision(2);
+cout<<c+d<<" "<<c-d<<endl;
+}
